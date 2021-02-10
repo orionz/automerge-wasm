c901bde9479ed0a027081b2047946f86b3f857ce
Orion Henry <orion.henry@gmail.com>
2021-02-10 13:30:59 -0500

    update deps

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 import * as Automerge from "automerge"
 import * as Backend from "automerge-backend-wasm-module"
 Automerge.setDefaultBackend(Backend)
+console.log(Automerge.init())
 export * from "automerge"
