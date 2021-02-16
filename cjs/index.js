93bec56d01ae11a5917b3a67cf9095b3494869f1
Orion Henry <orion.henry@gmail.com>
2021-02-16 14:17:45 -0500

    update to 0.1.4

--- cjs/index.js
+++ cjs/index.js
@@ -1,5 +1,4 @@
 let Automerge = require("automerge")
 let Backend = require("automerge-backend-wasm");
 Automerge.setDefaultBackend(Backend)
-console.log("AUTOMERGE", Automerge.init())
 module.exports = Automerge
--- mjs/index.js
+++ mjs/index.js
@@ -1,7 +1,6 @@
 import * as Automerge from "automerge"
 import * as Backend from "automerge-backend-wasm"
 Automerge.setDefaultBackend(Backend)
-console.log("AutomergeMJS",Automerge.init())
 export default Automerge
 export { Backend }
 export * from "automerge"
